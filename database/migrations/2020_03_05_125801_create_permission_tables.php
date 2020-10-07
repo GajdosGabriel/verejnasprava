8673d22d4ee30553783fa259e2c3383651c2dfd8
GajdosGabriel <gajdosgabo@gmail.com>
2020-10-07 20:23:44 +0200

    Permission migrate seed

--- database/migrations/2020_03_05_125801_create_permission_tables.php
+++ database/migrations/2020_03_05_125801_create_permission_tables.php
@@ -175,7 +175,32 @@ foreach ($permissions as $permission => $category) {
 //
         \DB::table('model_has_permissions')->insert([
             [
+                'permission_id' => 1,
+                'model_type' => 'App\Models\User',
+                'model_id' => 1,
+            ],
+            [
+                'permission_id' => 2,
+                'model_type' => 'App\Models\User',
+                'model_id' => 1,
+            ],
+            [
+                'permission_id' => 3,
+                'model_type' => 'App\Models\User',
+                'model_id' => 1,
+            ],
+            [
                 'permission_id' => 4,
+                'model_type' => 'App\Models\User',
+                'model_id' => 1,
+            ],
+            [
+                'permission_id' => 5,
+                'model_type' => 'App\Models\User',
+                'model_id' => 1,
+            ],
+            [
+                'permission_id' => 6,
                 'model_type' => 'App\Models\User',
                 'model_id' => 1,
             ],
