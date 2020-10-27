d00cc12733ed5dd2cfb6513337f69a886d0b75bb
GajdosGabriel <gajdosgabo@gmail.com>
2020-10-27 13:04:41 +0100

    User create fixing form

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -10,7 +10,7 @@
         <div class="md:w-1/2">
         <h1 class="font-bold text-2xl">Nový užívateľ</h1>
 
-        <form action="{{ route('user.store', [ auth()->user()->id, auth()->user()->slug ]) }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('user.store', [ auth()->user()->active_organization, auth()->user()->slug ]) }}" method="POST" enctype="multipart/form-data">
             @csrf @method('POST')
             @include('modul.errors')
             @include('user._userForm')
