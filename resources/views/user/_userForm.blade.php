d1d5813b8bd023ea813201c86121cbe79f12de05
GajdosGabriel <gajdosgabo@gmail.com>
2021-04-20 14:28:14 +0200

    admin form

--- resources/views/user/_userForm.blade.php
+++ resources/views/user/_userForm.blade.php
@@ -8,7 +8,7 @@
                         <label for="first_name" class="input-label">Meno</label>
 
                         <div class="col-md-8">
-                            <input id="first_name" type="text" class="input-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') ?? $user->first_name }}" required autocomplete="name" autofocus>
+                            <input id="first_name" type="text" class="w-full @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') ?? $user->first_name }}" required autocomplete="name" autofocus>
 
                             @error('first_name')
                                 <span class="invalid-feedback" role="alert">
@@ -23,7 +23,7 @@
                         <label for="last_name" class="input-label">Priezvisko</label>
 
                         <div class="col-md-8">
-                            <input id="last_name" type="text" class="input-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') ?? $user->last_name }}" required autocomplete="last_name">
+                            <input id="last_name" type="text" class="w-full @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') ?? $user->last_name }}" required autocomplete="last_name">
 
                             @error('first_name')
                             <span class="invalid-feedback" role="alert">
@@ -38,7 +38,7 @@
                             <label for="email" class="input-label">Email</label>
 
                             <div class="col-md-8">
-                                <input id="email" type="email" class="input-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') ?? $user->email}}" required autocomplete="email">
+                                <input id="email" type="email" class="w-full @error('email') is-invalid @enderror" name="email" value="{{ old('email') ?? $user->email}}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -53,7 +53,7 @@
                         <label for="employment" class="input-label">Funkcia</label>
 
                         <div class="col-md-8">
-                            <input id="employment" type="text" class="input-control @error('employment') is-invalid @enderror" name="employment" value="{{ old('employment') ?? $user->employment }}" autocomplete="employment">
+                            <input id="employment" type="text" class="w-full @error('employment') is-invalid @enderror" name="employment" value="{{ old('employment') ?? $user->employment }}" autocomplete="employment">
 
                             @error('employment')
                             <span class="invalid-feedback" role="alert">
