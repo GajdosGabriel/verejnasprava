26a4408af9bfe715706db6d36954ed3abf1c5df7
Gabriel Gajdoš <gajdosgabo@gmail.com>
2021-08-12 06:32:07 +0200

    chyba can lebo auth

--- app/Http/Controllers/Api/OrganizationUserController.php
+++ app/Http/Controllers/Api/OrganizationUserController.php
@@ -7,9 +7,15 @@ use App\Models\Organization;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use App\Http\Resources\UserCollection;
 
 class OrganizationUserController extends Controller
 {
+    public function index(Organization $organization)
+    {
+        return UserResource::collection($organization->users);
+    }
+
     public function show(Organization $organization, User $user)
     {
         return new UserResource($user);
