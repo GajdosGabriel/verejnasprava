10c9f038bbb84a648fe1170d703985beafe69e25
Gabriel Gajdoš <gajdosgabo@gmail.com>
2021-09-06 19:28:47 +0200

    Organization policy len file

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -34,6 +34,15 @@ class PostResource extends JsonResource
             // 'testovanie'        => $this->when( auth()->user()->can('create', App\Models\Post::class), 'niečo' ),
 
             'links' => [
+                // 'edit' => [
+                //     'name' => 'Upraviť',
+                //     'url' => 'http://Upraviť',
+                // ],
+
+                // 'delete' => [
+                //     'name' => 'Zmazať',
+                //     'url' => 'http://Upraviť',
+                // ]
                 // 'view'              => $this->when($request->user()->can('view', $request->user()), route('organizations.users.create', [$this->active_organization])),
                 // 'edit'              => $this->when($request->user()->can('update', $request->user()), route('organizations.users.edit', [$this->active_organization, $this->active_organization])),
                 // 'destroy'           => $this->when($request->user()->can('destroy', $request->user()), route('organizations.posts.update', [$this->organization_id, $this->id])),
