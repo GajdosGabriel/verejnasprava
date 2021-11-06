2a54d948f9da424d4dea4a4b976be4b3a6dbb00d
Gabriel Gajdoš <gajdosgabo@gmail.com>
2021-11-06 08:26:56 +0100

    Item fix meetings

--- resources/views/council/index.blade.php
+++ resources/views/council/index.blade.php
@@ -1,27 +1,28 @@
 @extends('layouts.app')
 @section('page-title', 'Zoznam zastupiteľstiev')
 
-    @section('navigation') @include('organizations.navigation') @endsection
+@section('navigation') @include('organizations.navigation') @endsection
 
 @section('content')
 
 
     <x-page.container>
-        <div class="w-2/3">
+        <x-page.page3_3>
+            <div class="col-span-9 bg-white p-3">
 
-            <council-table></council-table>
+                <council-table></council-table>
 
-            {{-- Show only first councils --}}
-            @if (!$councils->count() > 0)
-
-                @role('admin')
-                <a class="btn btn-primary float-right"
-                    href="{{ route('organizations.councils.create', auth()->user()->active_organization) }}">Nové
-                    zastupiteľstvo</a>
-                @endrole
-            @endif
-        </div>
-
+                {{-- Show if any councils dont exists --}}
+                @if (!$councils->count() > 0)
+                    @role('admin')
+                        <a class="btn btn-primary float-right"
+                            href="{{ route('organizations.councils.create', auth()->user()->active_organization) }}">
+                            Nové zastupiteľstvo
+                        </a>
+                    @endrole
+                @endif
+            </div>
+        </x-page.page3_3>
     </x-page.container>
 
 
