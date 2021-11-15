33b57caf5fc4f6c8216c48e4285c285be6bc73db
Gabriel Gajdoš <gajdosgabo@gmail.com>
2021-11-15 20:59:03 +0100

    Fix anyView all policy

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,30 +2,28 @@
 
 @section('page-title', 'Home profil')
 
-    @section('navigation') @include('organizations.navigation') @endsection
+@section('navigation') @include('organizations.navigation') @endsection
 
-    {{-- @section('navigation') @include('user.navigation') @endsection --}}
+{{-- @section('navigation') @include('user.navigation') @endsection --}}
 
 @section('content')
 
     <x-page.container>
 
-        <div class="">
+        <x-page.page-title>
+            <x-slot name="title">
+                Všetci užívatelia
+            </x-slot>
 
-            <x-page.page-title>
-                <x-slot name="title">
-                    Všetci užívatelia
-                </x-slot>
-
-                @role('admin')
+            @role('admin')
                 <a class="float-right btn btn-primary" href="{{ route('users.create') }}">Nový člen
                 </a>
-                @endrole
-            </x-page.page-title>
+            @endrole
+        </x-page.page-title>
 
 
-            @include('user._userTable')
-        </div>
+        @include('user._userTable')
+
     </x-page.container>
 
 @endsection
