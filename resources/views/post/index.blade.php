223ede53507ac6b53ad11c3d170e6f7933a14e84
Gabriel Gajdoš <gajdosgabo@gmail.com>
2021-11-17 08:53:35 +0100

    Post index component

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,7 +4,7 @@
 @section('navigation') @include('organizations.navigation') @endsection
 @section('content')
 
-<x-page.container>
+    <x-page.container>
 
         <div class="w-full">
 
@@ -15,21 +15,22 @@
                 </x-slot>
 
                 @role('admin')
-                <a class="btn btn-primary"
-                   href="{{ route('posts.create') }}">Nový doklad
-                </a>
+                    <a class="btn btn-primary" href="{{ route('posts.create') }}">Nový doklad
+                    </a>
                 @endrole
 
             </x-page.page-title>
 
+            <x-page.page3_3>
+                <div class="col-span-12 bg-white p-3">
+                    <post-table />
+                </div>
+            </x-page.page3_3>
 
-            <post-table/>
-
-
-            {{--            @include('post._table_index')--}}
-            {{--            <div class="my-6">--}}
-            {{--                {{ $posts->links() }}--}}
-            {{--            </div>--}}
+            {{-- @include('post._table_index') --}}
+            {{-- <div class="my-6"> --}}
+            {{-- {{ $posts->links() }} --}}
+            {{-- </div> --}}
 
         </div>
     </x-page.container>
