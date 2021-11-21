10371cc15891cdf80229b5f050c57f33d125abe9
Gabriel Gajdoš <gajdosgabo@gmail.com>
2021-11-21 20:12:34 +0100

    Council Resource create

---
+++ app/Http/Resources/CouncilResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CouncilResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
