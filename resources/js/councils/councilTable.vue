a707303666e5ac42da4ddbb8efce9da71b776144
Gabriel Gajdoš <gajdosgabo@gmail.com>
2021-12-01 10:34:29 +0100

    Council drop down

--- resources/js/councils/councilTable.vue
+++ resources/js/councils/councilTable.vue
@@ -14,114 +14,6 @@
                         :items="council"
                         @fromItem="clickOnItem"
                     ></drop-down-component>
-
-                    <nav-drop-down v-if="$auth.can('council delete')">
-                        <slot>
-                            <a
-                                class="
-                                    block
-                                    px-4
-                                    py-2
-                                    text-sm
-                                    leading-5
-                                    text-gray-700
-                                    hover:bg-gray-100 hover:text-gray-900
-                                    focus:outline-none
-                                    focus:bg-gray-100
-                                    focus:text-gray-900
-                                    whitespace-no-wrap
-                                "
-                                :href="
-                                    'council/' + council.id + '/meeting/create'
-                                "
-                                title="Vytvoriť nové zasadnutie"
-                            >
-                                <div class="flex">
-                                    <svg
-                                        class="w-4 h-4 mr-2"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20"
-                                    >
-                                        <path
-                                            d="M9 10V8h2v2h2v2h-2v2H9v-2H7v-2h2zm-5 8h12V6h-4V2H4v16zm-2 1V0h12l4 4v16H2v-1z"
-                                        />
-                                    </svg>
-                                    Nové zasadnutie
-                                </div>
-                            </a>
-
-                            <a
-                                class="
-                                    cursor-pointer
-                                    block
-                                    px-4
-                                    py-2
-                                    text-sm
-                                    leading-5
-                                    text-gray-700
-                                    hover:bg-gray-100 hover:text-gray-900
-                                    focus:outline-none
-                                    focus:bg-gray-100
-                                    focus:text-gray-900
-                                    whitespace-no-wrap
-                                "
-                                title="Upraviť položku"
-                                @click="modalEdit(council)"
-                            >
-                                <div class="flex">
-                                    <svg
-                                        class="w-4 h-4 mr-2"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20"
-                                    >
-                                        <path
-                                            d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"
-                                        />
-                                    </svg>
-                                    Upraviť položku
-                                </div>
-                            </a>
-
-                            <div
-                                class="
-                                    block
-                                    px-4
-                                    py-2
-                                    cursor-pointer
-                                    text-sm
-                                    leading-5
-                                    text-gray-700
-                                    hover:bg-gray-100 hover:text-gray-900
-                                    focus:outline-none
-                                    focus:bg-gray-100
-                                    focus:text-gray-900
-                                    whitespace-no-wrap
-                                "
-                                @click="modalDelete(council)"
-                                :href="
-                                    'admin/' +
-                                    council.id +
-                                    '/' +
-                                    council.slug +
-                                    '/council/delete'
-                                "
-                                title="Zmazať zastupiteľstvo"
-                            >
-                                <div class="flex">
-                                    <svg
-                                        class="w-4 h-4 mr-2"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 20 20"
-                                    >
-                                        <path
-                                            d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"
-                                        />
-                                    </svg>
-                                    Zmazať
-                                </div>
-                            </div>
-                        </slot>
-                    </nav-drop-down>
                 </div>
             </div>
             <div class="flex flex-col sm:ml-5">
@@ -183,14 +75,14 @@ export default {
     }),
 
     methods: {
-        clickOnItem(action, meeting) {
-            if (action == "delete") {
-                this.$store.dispatch(
-                    "councils/deleteCouncil",
-                    meeting.navigations.delete.url
-                );
+        clickOnItem(action, item) {
+            if (action == "edit") {
+                this.modalEdit(item);
             }
 
+            if (action == "delete") {
+                this.modalDelete(item);
+            }
         },
         modalEdit: function (item) {
             this.opencomponent = "modaledit";
